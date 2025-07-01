dd6194a3e56ac8d2111f3608c1abbd06ec3c95f9
infable1 <nqsarahos@mail.ru>
2025-07-01 18:27:26 +0300

    Getting a list of cities and creating a single index for date and time

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('weather_data.csv')
 df = df.dropna()
+
+cities = list(set(df['Location']))
+cities.sort()
+
+df['Date_Time'] = pd.to_datetime(df['Date'] + ' ' + df['Time'])
