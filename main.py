279d64c42b405978e416031206d5c2d82998d552
infable1 <nqsarahos@mail.ru>
2025-07-01 18:35:58 +0300

    Creating folders for processed data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 df = pd.read_csv('weather_data.csv')
 df = df.dropna()
@@ -9,3 +10,6 @@ cities = list(set(df['Location']))
 cities.sort()
 
 df['Date_Time'] = pd.to_datetime(df['Date'] + ' ' + df['Time'])
+
+for city in cities:
+    Path(f'Analyzed/{city}').mkdir(exist_ok=True)
