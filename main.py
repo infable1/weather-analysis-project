90b61a55ecf64cb578814a965b21eed38044ec71
infable1 <nqsarahos@mail.ru>
2025-07-01 21:30:20 +0300

    Importing libraries for working with Excel and creating a workbook

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
+from openpyxl import Workbook
+from openpyxl.styles import Font, Alignment
 
 df = pd.read_csv('weather_data.csv')
 df = df.dropna()
@@ -10,6 +12,16 @@ cities = list(set(df['Location']))
 cities.sort()
 
 df['Date_Time'] = pd.to_datetime(df['Date'] + ' ' + df['Time'])
+
+wb = Workbook()
+
+default_sheet = "Sheet"
+if default_sheet in wb.sheetnames:
+    sheet_to_remove = wb[default_sheet]
+    wb.remove(sheet_to_remove)
+
+wb.create_sheet('All month long')
+data_sheet = wb['All month long']
 
 for city in cities:
     Path(f'Analyzed/{city}').mkdir(exist_ok=True)
